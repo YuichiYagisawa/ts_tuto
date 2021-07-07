e01614f3f468f728c16275fb63cadc5b517e101c
Yuichi Yagisawa <yuichi9915@gmail.com>
2021-07-07 23:34:04 +0900

    first

---
+++ index.js
@@ -0,0 +1,14 @@
+var hasValue = true;
+var count = 10;
+var float = 0.9;
+var negative = -0.1;
+var single = 'hello';
+var double = "hello";
+var back = "hello";
+var hello;
+hello = 'hello';
+var person = {
+    name: 'jack',
+    age: 21
+};
+console.log(person);
---
+++ index.ts
@@ -0,0 +1,19 @@
+let hasValue = true;
+let count = 10;
+let float = 0.9;
+let negative = -0.1;
+let single = 'hello';
+let double = "hello";
+let back = `hello`;
+let hello: string;
+hello = 'hello';
+
+const person: {
+  name: string;
+  age: number;
+} = {
+  name: 'jack',
+  age: 21
+}
+
+console.log(person);
